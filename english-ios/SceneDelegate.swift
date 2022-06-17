db5a3ca223d9e23c5cbccc0e13359b175d2c94a6
polykuzin <polykuzin@gmail.com>
2022-06-17 19:40:28 +0300

    Initial Commit

---
+++ english-ios/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  english-ios
+//
+//  Created by polykuzin on 17/06/2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = ViewController()
+        self.window = window
+        window.makeKeyAndVisible()
+    }
+}
---
+++ english-ios/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  english-ios
+//
+//  Created by polykuzin on 17/06/2022.
+//
+
+import UIKit
+
+class ViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
